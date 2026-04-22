2ecab07348f5b52d2562b10ae9ce546a31333a64
Janushan Anandakumar <0073033@schoolsnet.act.edu.au>
2026-04-22 11:58:20 +1000

    Created Inventory User Control UI

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace WindowsFormsApp10
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
